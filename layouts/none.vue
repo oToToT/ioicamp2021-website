a38064fe892f4c5d52281705c7f9a638a46184c7
oToToT <ty1208chiang@gmail.com>
2020-09-24 03:53:20 +0800

    margin css should be in layout

--- layouts/none.vue
+++ layouts/none.vue
@@ -8,3 +8,22 @@ export default Vue.extend({
   name: 'None',
 })
 </script>
+
+<style lang="sass">
+@font-face {
+    font-family: "Huninn";
+    font-style: normal;
+    font-display: swap;
+    src: url('/jf-openhuninn-1.1.woff2') format("woff2"),
+        url('/jf-openhuninn-1.1.woff') format("woff"),
+        url('/jf-openhuninn-1.1.ttf') format("truetype");
+}
+* {
+  margin: 0;
+  font-family: "Huninn", "Noto Sans TC", sans-serif;
+}
+.fa, .far, .fas {
+  font-family: "Font Awesome 5 Free";
+}
+
+</style>
