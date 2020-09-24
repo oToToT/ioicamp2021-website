ca5a0bb6b585cf40f67eb8496109db43fb9ce446
oToToT <ty1208chiang@gmail.com>
2020-09-24 21:32:30 +0800

    add example courses

--- pages/event/courses.vue
+++ pages/event/courses.vue
@@ -1,20 +1,104 @@
 <template lang="pug">
-.courses
-  | meow
+.container
+  h1.title
+    i.fas.fa-book
+    | &nbsp;課程主題
+  .courses
+    section-block.course
+      template(v-slot:title) 課前自我鍛鍊
+      template
+        h4 先到各大 Online Judge 申請帳號寫題目吧！
+        ul
+          li
+            a(href='https://codeforces.com') Codeforces
+          li
+            a(href='https://atcoder.jp/') AtCoder
+          li
+            a(href='https://www.codechef.com/') CodeChef
+          li
+            a(href='https://www.topcoder.com/') TopCoder
+          li
+            a(href='https://tioj.infor.org/') TIOJ Infor Online Judge
+        h4 舒適的打比賽環境
+        ul
+          li 建置你的開發環境
+          li 測試比賽環境指南
+          li 點心 x 飲料 x 氣球
+        h4 預備的預備知識
+        ul
+          li 網路資源
+          li 你的好友
+    section-block(v-for="course in courses", :key="course.slug").course
+      template(v-slot:title) {{ course.name }}
+      template
+        h4 預備知識
+        ul
+          li(v-for="knowledge in course.preknowledge") {{ knowledge }}
+        h4 課程大綱
+        ul
+          li(v-for="content in course.abstract") {{ content }}
+  .cutter
 </template>
 
-<script lang="ts">
+<script>
 import Vue from "vue";
 export default Vue.extend({
   head: {
-    title: 'IOICcamp 2021 - 集訓內容',
+    title: "IOICcamp 2021 - 集訓內容",
     meta: [
-      { hid: 'description', name: 'description', content: 'Home page description' }
+      {
+        hid: "description",
+        name: "description",
+        content: "Home page description",
+      },
     ],
+  },
+  data() {
+    return {
+      courses: [],
+    };
+  },
+  async fetch() {
+    this.courses = await this.$content("courses").sortBy("slug", "asc").fetch();
   },
 });
 </script>
 
 <style lang="scss" scoped>
+.container {
+  padding-top: $nav-header-space;
+  padding-left: 10vw;
+  padding-right: 10vw;
+  @include with-mobile {
+    padding-top: $mobile-nav-header-space;
+    padding-left: 5vw;
+    padding-right: 5vw;
+  }
+  h1.title {
+    color: #ffe300;
+    text-shadow: -0.5px -0.5px 0 #000, 0.5px -0.5px 0 #000, -0.5px 0.5px 0 #000,
+      0.5px 0.5px 0 #000;
 
+    font-weight: 300;
+  }
+  .courses {
+    margin-top: 20px;
+    padding-left: 1vw;
+    padding-right: 1vw;
+    display: flex;
+    flex-wrap: wrap;
+    .course {
+      width: calc(50% - 8px);
+      &:nth-child(even) {
+        margin-left: 8px;
+      }
+      &:nth-child(odd) {
+        margin-right: 8px;
+      }
+    }
+  }
+  .cutter {
+    height: 1px;
+  }
+}
 </style>
