cdcfe49bb57b708a566ebbab851d1f9fd20d9e14
oToToT <ty1208chiang@gmail.com>
2020-11-01 23:22:55 +0800

    add sponsors page

---
+++ pages/sponsors.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+.container
+  nuxt-section-block(
+    v-for="sponsor in sponsors",
+    :id="sponsor.id",
+    :document="sponsor",
+    :key="sponsor.id"
+  )
+  .cutter
+</template>
+
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  head: {
+    title: "贊助夥伴",
+  },
+  data() {
+    return {
+      sponsors: [],
+    };
+  },
+  async fetch() {
+    this.sponsors = await this.$content("sponsors").sortBy("prio", "desc").sortBy("createdAt", "desc").fetch();
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.container {
+  padding-top: $nav-header-space;
+  padding-left: 10vw;
+  padding-right: 10vw;
+  @include with-mobile {
+    padding-top: $mobile-nav-header-space;
+    padding-left: 5vw;
+    padding-right: 5vw;
+  }
+  .cutter {
+    height: 1px;
+  }
+}
+</style>
